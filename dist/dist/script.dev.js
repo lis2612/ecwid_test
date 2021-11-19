5ecee2d29c01a6b2ccb0bf812d814e7ad4eeaa6c
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2021-11-19 16:22:30 +0400

    sending email

--- dist/dist/script.dev.js
+++ dist/dist/script.dev.js
@@ -7,15 +7,10 @@ var userPhone = document.getElementById('inpTel');
 var userEmail = document.getElementById('inpEmail');
 var userText = document.getElementById('inpText');
 var btnSubmit = document.getElementById('btnSubmit');
+var templateHTML = ['<!DOCTYPE html> \n', '  <html lang="en"> \n', '    <head> \n', '      <meta charset="utf-8"> \n', '      <title>Обратная связь</title> \n', '    </head> \n', '    <body> \n', '      <div id="formWrapper"> \n', '        <div id="formLabel">Напишите нам</div> \n', '        <form id="formFB"> \n', '          <label for="inpName">Имя</label> \n', '          <input id="inpName" name="Name" type="text" placeholder="Кот Котофеич"> \n', '          <label for="inpBday">Дата рождения</label> \n', '          <input id="inpBday" name="Birthday" type="date"> \n', '          <label for="inpTel">Телефон</label> \n', '          <input id="inpTel" name="Phone" type="tel" placeholder="+78001234567" maxlength="14"> \n', '          <label for="inpEmail">Почта</label> \n', '          <input id="inpEmail" name="Email" type="email" placeholder="kotofei@catmail.com"> \n', '          <label for="inpText">Ваш вопрос:</label> \n', '          <textarea id="inpText" name="Text" placeholder="А мыши кто?"></textarea> \n', '          <input id="btnSubmit" name="btnSubmit" type="submit" value="ОТПРАВИТЬ"> \n', '        </form> \n', '      </div> \n', '    </body> \n', '  </html>'].join('');
 
 function popupSubmit(event) {
   event.preventDefault();
-  console.clear;
-  console.log('Name ' + userName.style.borderColor);
-  console.log('BDay ' + userBDay.style.borderColor);
-  console.log('Phone ' + userPhone.style.borderColor);
-  console.log('Email ' + userEmail.style.borderColor);
-  console.log('Text ' + userText.style.borderColor);
 
   if (!userName.value || !userBDay.value || !userPhone.value || !userEmail.value || !userText.value) {
     alert('Заполните форму!');
@@ -28,9 +23,20 @@ function popupSubmit(event) {
   }
 
   if (userName.style.borderColor != 'red' && userBDay.style.borderColor != 'red' && userPhone.style.borderColor != 'red' && userEmail.style.borderColor != 'red' && userText.style.borderColor != 'red') {
-    var form = document.forms.formFB;
-    alert(form.inpName.value + '\n' + form.inpBday.value + '\n' + form.inpTel.value + '\n' + form.inpEmail.value + '\n' + form.inpText.value);
-    return;
+    //Get data from form
+    //let form = document.forms.formFB;
+    Email.send({
+      // Host: 'smtp.mail.ru',
+      // Username: 'testmail87@internet.ru',
+      // Password: 'JViT2FZndzPjKVhvmq9X',
+      SecureToken: 'a5e2add3-4073-485a-b25c-3972602d89b0',
+      To: 'lyamin.ilya.s@gmail.com',
+      From: 'testmail87@internet.ru',
+      Subject: 'Вам что-то оставили...',
+      Body: templateHTML
+    }).then(function (message) {
+      return alert(message);
+    });
   }
 }
 
